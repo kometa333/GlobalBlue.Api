e7c58bad80f5d6cb79c90370bb8cf87f949a1223
e.gorcsosova <e.gorcsosova@ctidigital.com>
2025-04-22 13:47:52 +0200

    Swagger config updated

--- Program.cs
+++ Program.cs
@@ -11,12 +11,9 @@ builder.Services.AddScoped<GlobalBlue.Api.Services.Interfaces.IVatCalculator, Gl
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
